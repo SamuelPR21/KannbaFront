963b705c5fbbbfaec231b37fc7b209c96db2ddad
Samuel A. Perdomo Fajardo <u20222208795@usco.edu.co>
2025-10-20 11:08:05 -0500

    Inicializacion del proyecto

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+  
+
+    <View className="flex-1 justify-center items-center"
+    >
+      <Text className="text-blue-500">Hola, NativeWind!</Text>
+      <Text className="text-red-500"> Edit app/index.tsx to edit this screen. </Text>
+    </View>
+  );
+}
